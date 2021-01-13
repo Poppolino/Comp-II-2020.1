f05ca40860c57968b65df3f2b9d340a92245a6e6
Poppolino <57730364+Poppolino@users.noreply.github.com>
2021-01-12 23:20:49 -0300

    Rename Lab 4/Album.java to Lab 4/src/Album.java

--- Lab 4/src/Album.java
+++ Lab 4/src/Album.java
@@ -12,12 +12,12 @@ public class Album {
     
 
     public Album(int tamanhoDoAlbum, int quantFigurinhasPorPacotinho) {
-        if(tamanhoDoAlbum <= 0){                                        // verifica se o tamanho È consistente
-            throw new RuntimeException("O ·lbum deve ter pelo menos uma figurinha!");
+        if(tamanhoDoAlbum <= 0){                                        // verifica se o tamanho √© consistente
+            throw new RuntimeException("O √°lbum deve ter pelo menos uma figurinha!");
         }
         
         if(quantFigurinhasPorPacotinho <= 0){                           // verifica se a quantidade de figurinhas faz sentido
-            throw new RuntimeException("O pacote deve ter no mÌnimo uma figurinha!");
+            throw new RuntimeException("O pacote deve ter no m√≠nimo uma figurinha!");
         }
         
         this.tamanhoDoAlbum = tamanhoDoAlbum;
@@ -29,14 +29,14 @@ public class Album {
 
     
     /**
-     * Recebe novo pacote de figurinhas e cola elas no ·lbum ou bota no monte de repetidas 
+     * Recebe novo pacote de figurinhas e cola elas no √°lbum ou bota no monte de repetidas 
      * @param pacotinho
      */
     public void receberNovoPacotinho(Pacotinho pacotinho) {
         for (Figurinha fig : pacotinho) {
             int posicao = fig.getPosicao();
             
-            if(possuiFigurinhaColada(posicao)){              // Se j· foi colada, vai para o monte das repetidas
+            if(possuiFigurinhaColada(posicao)){              // Se j√° foi colada, vai para o monte das repetidas
                 this.figurinhasRepetidas.add(fig);
                 continue;
             }
@@ -48,12 +48,12 @@ public class Album {
     
 
     /**
-     * Preenche o ·lbum com as figurinhas que faltam para complet·-lo
+     * Preenche o √°lbum com as figurinhas que faltam para complet√°-lo
      */
     public void autoCompletar() {
         int completo = (this.quantFigurinhasColadas*100) / this.tamanhoDoAlbum; 
         
-        if(completo < PERCENTUAL_MINIMO_PARA_AUTO_COMPLETAR){           //Se n„o atingiu o percentual mÌnimo, n„o pode autocompletar
+        if(completo < PERCENTUAL_MINIMO_PARA_AUTO_COMPLETAR){           //Se n√£o atingiu o percentual m√≠nimo, n√£o pode autocompletar
             return;
         }
 
@@ -68,9 +68,9 @@ public class Album {
     
 
     /**
-     * Verifica se uma figurinha j· foi colada no ·lbum
+     * Verifica se uma figurinha j√° foi colada no √°lbum
      * @param posicao
-     * @return verdadeiro se a figurinha est· colada, ou falso caso contr·rio
+     * @return verdadeiro se a figurinha est√° colada, ou falso caso contr√°rio
      */
     public boolean possuiFigurinhaColada(int posicao) {
         if(posicao < 1 || posicao > this.tamanhoDoAlbum){
@@ -84,7 +84,7 @@ public class Album {
     /**
      * Verifica se possui uma certa figurinha repetida
      * @param posicao
-     * @return verdadeiro se tem uma repetida, ou falso caso contr·rio
+     * @return verdadeiro se tem uma repetida, ou falso caso contr√°rio
      */
     public boolean possuiFigurinhaRepetida(int posicao) {
         if(posicao < 1 || posicao > this.tamanhoDoAlbum){
@@ -96,35 +96,35 @@ public class Album {
 
     
     /**
-     * @return tamanho do ·lbum
+     * @return tamanho do √°lbum
      */
     public int getTamanho() {
         return this.tamanhoDoAlbum;
     }
 
     /**
-     * @return quantidade de figurinhas por pacote do ·lbum  
+     * @return quantidade de figurinhas por pacote do √°lbum  
      */
     public int getQuantFigurinhasPorPacotinho() {
         return this.quantFigurinhasPorPacotinho;
     }
     
     /**
-     * @return quantidade de figurinhas coladas no ·lbum
+     * @return quantidade de figurinhas coladas no √°lbum
      */
     public int getQuantFigurinhasColadas() {
         return this.quantFigurinhasColadas;
     }
 
     /**
-     * @return a quantidade de figurinhas que s„o repetidas
+     * @return a quantidade de figurinhas que s√£o repetidas
      */
     public int getQuantFigurinhasRepetidas() {
         return this.figurinhasRepetidas.size();
     }
 
     /**
-     * @return a quantidade de figurinhas que faltam para completar o ·lbum
+     * @return a quantidade de figurinhas que faltam para completar o √°lbum
      */
     public int getQuantFigurinhasFaltantes() {
         return this.tamanhoDoAlbum - this.quantFigurinhasColadas;
